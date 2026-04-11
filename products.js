e4961b3370765f2f3c4102b669cc8a02bac58398
davidizdead <dawud.anon@outlook.com>
2026-04-11 05:56:33 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,10 +1,10 @@
 const SKIZA_PRODUCTS = [
   {
-    "id": 1775838581368,
-    "name": "Kashmiri Shawl",
-    "category": "Fashion",
-    "price": 2900,
-    "discount": 0,
+    "id": 1775867175568,
+    "name": "Daal Chini",
+    "category": "Food & Groceries",
+    "price": 450,
+    "discount": 10,
     "location": "Lal Chowk, Srinagar",
     "stock": 12,
     "image": "images/p1.jpg",
@@ -13,8 +13,8 @@ const SKIZA_PRODUCTS = [
     "commission": 10
   },
   {
-    "id": 1775838581370,
-    "name": "Kashmiri Tea",
+    "id": 1775867175570,
+    "name": "Makhan malai",
     "category": "Food & Groceries",
     "price": 450,
     "discount": 10,
@@ -26,27 +26,14 @@ const SKIZA_PRODUCTS = [
     "commission": 10
   },
   {
-    "id": 1775838581371,
-    "name": "Nun Chai",
-    "category": "Food & Groceries",
+    "id": 1775867175571,
+    "name": "Hrlicopter",
+    "category": "Toys",
     "price": 450,
     "discount": 10,
     "location": "Lal Chowk, Srinagar",
     "stock": 12,
     "image": "images/p3.jpg",
-    "seller": "Kashmir Dry Fruits",
-    "whatsapp": "9596883003",
-    "commission": 10
-  },
-  {
-    "id": 1775838581372,
-    "name": "Lipton chai",
-    "category": "Food & Groceries",
-    "price": 450,
-    "discount": 10,
-    "location": "Lal Chowk, Srinagar",
-    "stock": 12,
-    "image": "images/p4.jpg",
     "seller": "Kashmir Dry Fruits",
     "whatsapp": "9596883003",
     "commission": 10
